6ef3d20628d962c14a64b716ed3a10f7778a680d
Dmitry <jump.aroundtheworld@mail.ru>
2023-03-29 21:34:35 +0300

    (stage3) add file1, add file2, add comparison.js, add index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { getDifferent } from '../src/index.js';
+
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .version('0.0.1')
+  .action((filepath1, filepath2) => console.log(getDifferent(filepath1, filepath2)));
   program.parse();
--- gendiff.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const { Command } = import('commander');
-const program = new Command();
-
-program
-  .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
-  program.parse();
